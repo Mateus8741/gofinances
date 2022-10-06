a77bb2705e40a0cba6b16030f623c777794487d5
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-10-06 13:04:06 -0300

    auth test

---
+++ src/hooks/auth.spec.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { AuthProvider, useAuth } from "./auth";
+
+describe("Auth Hook", () => {
+  it("Google account login", () => {
+    
+  });
+});
