6b36fcd662e9a18099cec8134b91db00932ee7eb
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-10-07 03:52:24 -0300

    credentials

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { AuthProvider, useAuth } from "./auth";
 
+jest.mock("expo-auth-session", () => {
+  return {
+    startAsync: () => {
+      return {
+        type: "success",
+        params: {
+          access_token: "google-token",
+        },
+      };
+    },
+  };
+});
+
 describe("Auth Hook", () => {
-  it("Google account login", () => {
-    
+  it("Google account login", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            id: `userInfo.id`,
+            email: `userInfo.email`,
+            name: `userInfo.given_name`,
+            photo: `userInfo.picture`,
+            locale: `userInfo.locale`,
+            verified_email: `userInfo.verified_email`,
+          }),
+      })
+    ) as jest.Mock;
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).toBeTruthy();
   });
 });
